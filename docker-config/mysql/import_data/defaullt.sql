e4a4bbe5fbe4276f51775906bb84136da996056e
sho55 <gra2546nd@gmail.com>
2023-02-07 22:15:24 +0900

    :hammer: SQLコード修正

--- docker-config/mysql/import_data/defaullt.sql
+++ docker-config/mysql/import_data/defaullt.sql
@@ -13,7 +13,6 @@ CREATE TABLE `posts` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`title` VARCHAR(255) DEFAULT 'no title',
 	`body` VARCHAR(1024) NOT NULL,
-	`image` MEDIUMBLOB,
 	`created_at` datetime  default current_timestamp,
 	`updated_at` timestamp default current_timestamp on update current_timestamp,
 	PRIMARY KEY (`id`)
